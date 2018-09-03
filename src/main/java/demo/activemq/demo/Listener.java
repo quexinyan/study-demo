613472719cb32441e42dd113ecafead278ba0671
quexinyan <982175620@qq.com>
2018-09-03 23:12:33 +0800

    activeMq

---
+++ src/main/java/demo/activemq/demo/Listener.java
@@ -0,0 +1,26 @@
+package demo.activemq.demo;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ * 消息监听
+ * @author Administrator
+ *
+ */
+public class Listener implements MessageListener{
+
+	@Override
+	public void onMessage(Message message) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("收到的消息："+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/demo/fenye/ListPage.java
+++ src/main/java/demo/fenye/ListPage.java
@@ -14,7 +14,7 @@ public class ListPage {
 
 	public static void main(String[] args) {
 		
-		List<Integer> list = new ArrayList<>();
+		List<Integer> list = new ArrayList<Integer>();
 		for(int i = 1; i <= 30; i++) {
 			list.add(i);
 		}
