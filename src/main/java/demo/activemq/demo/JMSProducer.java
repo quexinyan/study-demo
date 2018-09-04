fb4c972266af3eccee0daf4c87aa90a7c7c641a6
quexinyan <982175620@qq.com>
2018-09-04 22:56:34 +0800

    activeMq

--- src/main/java/demo/activemq/demo/JMSProducer.java
+++ src/main/java/demo/activemq/demo/JMSProducer.java
@@ -24,14 +24,30 @@ public class JMSProducer {
         MessageProducer messageProducer; // 消息生产者
 
         // 实例化连接工厂
+        //第一步：建立ConnectionFactory工厂对象，需要填入用户名、密码、以及要连接的地址，均使用默认即可，默认端口为"tcp://localhost:61616"
         connectionFactory=new ActiveMQConnectionFactory(JMSProducer.USERNAME, JMSProducer.PASSWORD, JMSProducer.BROKEURL);
 
         try {
+
+            //第二步：通过ConnectionFactory工厂对象我们创建一个Connection连接，并且调用Connection的start方法开启连接，Connection默认是关闭的。
             connection=connectionFactory.createConnection(); // 通过连接工厂获取连接
             connection.start(); // 启动连接
+
+            //第三步：通过Connection对象创建Session会话（上下文环境对象），用于接收消息，参数配置1为是否启用是事务，参数配置2为签收模式，一般我们设置自动签收。
             session=connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE); // 创建Session
+
+            //第四步：通过Session创建Destination对象，指的是一个客户端用来指定生产消息目标和消费消息来源的对象，在PTP模式中，Destination被称作Queue即队列；
+            // 在Pub/Sub模式，Destination被称作Topic即主题。在程序中可以使用多个Queue和Topic。
             destination=session.createQueue("FirstQueue2"); // 创建消息队列
+
+            //第五步：我们需要通过Session对象创建消息的发送和接收对象（生产者和消费者）MessageProducer/MessageConsumer。
             messageProducer=session.createProducer(destination); // 创建消息生产者
+
+            //第六步：我们可以使用MessageProducer的setDeliveryMode方法为其设置持久化特性和非持久化特性（DeliveryMode），我们稍后详细介绍。
+            //messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            //第七步：最后我们使用JMS规范的TextMessage形式创建数据（通过Session对象），并用MessageProducer的send方法发送数据。
+            // 同理客户端使用receive方法进行接收数据。最后不要忘记关闭Connection连接。
             sendMessage(session, messageProducer); // 发送消息
             session.commit();	// 由于创建session时，开启了事务，所以要commit才能完成
         } catch (Exception e) {
